bc97047a75b14fade96c350a2fe03cdcbe344a36
levisonjr <levisonjr21@gmail.com>
2024-09-01 18:44:20 -0300

    Estabelecendo rota para a MainPage (MainComponent)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
+    {
+        path: "",
+        component: MainComponent
+    },
     {
         path: "login",
         component: LoginComponent
