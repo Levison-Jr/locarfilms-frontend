fc2f0897644ad32a72fdb6d6ffa01106671f1d50
levisonjr <levisonjr21@gmail.com>
2024-09-03 23:25:57 -0300

    Implementando componente carousel

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { DefaultLayoutComponent } from '../../components/default-layout/default-layout.component';
+import { NgOptimizedImage } from '@angular/common';
+import { CarouselComponent } from '../../components/carousel/carousel.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
-    DefaultLayoutComponent
+    DefaultLayoutComponent,
+    NgOptimizedImage,
+    CarouselComponent
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
+  images = [
+    {
+      imageSrc: "https://dspa.com.br/wp-content/uploads/2018/04/1d69b1e3d517074c4643f77f95a64f49.jpg"
+    },
+    {
+      imageSrc: "https://fly.metroimg.com/upload/q_85,w_700/https://uploads.metroimg.com/wp-content/uploads/2024/03/07195642/Divertida-Mente-2.jpg"
+    },
+    {
+      imageSrc: "https://cinepop.com.br/wp-content/uploads/2020/02/um-lugar-silencioso-parte-2.jpg"
+    }
+  ];
 }
