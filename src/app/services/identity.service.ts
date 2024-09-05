57c7604f3af831c90ebb509368a715ac3cdd8bbd
levisonjr <levisonjr21@gmail.com>
2024-09-04 21:35:32 -0300

    Utilizando o catchError nas requisições de cadastro

--- src/app/services/identity.service.ts
+++ src/app/services/identity.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/response/login-response.type';
-import { tap } from 'rxjs';
+import { catchError, tap } from 'rxjs';
 import { UserDto } from '../types/response/user-dto.type';
+import { ErrorHandlerService } from './error-handler.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class IdentityService {
   endpointLogin: string = "https://localhost:7141/api/Users/login";
   endpointCadastro: string = "https://localhost:7141/api/Users/register";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private errorHandler: ErrorHandlerService) { }
 
   login(email: string, password: string) {
     return this.httpClient.post<LoginResponse>(this.endpointLogin, { email, password })
@@ -25,11 +26,14 @@ export class IdentityService {
 
   cadastro(email: string, password: string, passwordConfirm: string) {
     return this.httpClient.post<UserDto>(this.endpointCadastro, { email, password, passwordConfirm })
-      .pipe(tap(
-        (value) => {
-          console.log(value);
-          sessionStorage.setItem("last-registered-user-email", value.email)
-        })
+      .pipe(
+        tap(
+          (value) => {
+            console.log(value);
+            sessionStorage.setItem("last-registered-user-email", value.email)
+          }
+        ),
+        catchError((error) => this.errorHandler.handleError(error))
       );
   }
 }
