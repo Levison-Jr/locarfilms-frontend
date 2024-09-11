e4ee20050c70abbc58d9e46b44b762c67f793dda
levisonjr <levisonjr21@gmail.com>
2024-09-10 22:15:05 -0300

    Layout estático completo da página de detalhes dos filmes

--- src/app/pages/info-movie/info-movie.component.ts
+++ src/app/pages/info-movie/info-movie.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DefaultLayoutComponent } from '../../components/default-layout/default-layout.component';
 import { NgOptimizedImage } from '@angular/common';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-info-movie',
   standalone: true,
   imports: [
     DefaultLayoutComponent,
-    NgOptimizedImage
+    NgOptimizedImage,
+    ButtonComponent
   ],
   templateUrl: './info-movie.component.html',
   styleUrl: './info-movie.component.scss'
