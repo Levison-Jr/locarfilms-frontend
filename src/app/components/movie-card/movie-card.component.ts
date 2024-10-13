9e7858b74d43d75c43f1f7bfeab2d7c660b61f2d
levisonjr <levisonjr21@gmail.com>
2024-10-13 01:28:21 -0300

    Add função de navegação para pagina do filme ao clicar no componente

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,9 +1,13 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
+  host: {
+    '(click)':'navigateToMoviePage()'
+  },
   imports: [
     NgOptimizedImage
   ],
@@ -15,4 +19,10 @@ export class MovieCardComponent {
   @Input() titulo: string = "";
   @Input() categoria: string = "";
   @Input() caminhoImagem: string = "";
+
+  constructor(private router: Router) { }
+
+  navigateToMoviePage() {
+    this.router.navigate([`filme/${this.movieId}`]);
+  }
 }
