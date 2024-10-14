0c2d1ecf1995a6aa3afbe50b4a6489ccd3940783
levisonjr <levisonjr21@gmail.com>
2024-10-14 20:34:11 -0300

    Default component: Página dos fimes alugados pelo usuário

---
+++ src/app/pages/user-movies/user-movies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-movies',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-movies.component.html',
+  styleUrl: './user-movies.component.scss'
+})
+export class UserMoviesComponent {
+
+}
