0c6b2ed67ed8b9760c26f03a09df44616743fda9
levisonjr <levisonjr21@gmail.com>
2024-10-30 22:11:25 -0300

    Estrutura inicial feita com busca das info do user pelo id

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { DefaultLayoutComponent } from '../../components/default-layout/default-layout.component';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { UserService } from '../../services/user.service';
+import { UserDto } from '../../types/response/user-dto.type';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    DefaultLayoutComponent,
+    PrimaryInputComponent
+  ],
+  providers: [
+    UserService
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
 
+  constructor(
+    private userService: UserService,
+    private toastr: ToastrService) { }
+
+  user!: UserDto;
+
+  ngOnInit() {
+    const userId = sessionStorage.getItem("user-id") || "";
+    this.userService.buscarPeloId(userId).subscribe({
+      next: (value) => {
+        this.user = value;
+      },
+      error: (error) => {
+        this.toastr.error(error.message, "FALHA");
+      }
+    });
+  }
 }
